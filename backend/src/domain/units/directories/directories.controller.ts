f7184c6d54510f416ae03d8ae2811a74aa0362e9
Breno Salles <git@brenosalles.com>
2022-05-22 18:01:43 +0100

    feat: Delete directory

--- backend/src/domain/units/directories/directories.controller.ts
+++ backend/src/domain/units/directories/directories.controller.ts
@@ -1,10 +1,21 @@
 import { User } from "@entities/user.entity";
 import { UserDecorator } from "@generics/User.decorator";
 import { BothAuthGuard } from "@guards/both-auth.guard";
-import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from "@nestjs/common";
 import { ApiTags, ApiSecurity, ApiBearerAuth } from "@nestjs/swagger";
 import { CreateDirectoryDto } from "./use-cases/create-directory/create-directory.dto";
 import { CreateDirectoryService } from "./use-cases/create-directory/create-directory.service";
+import { DeleteDirectoryService } from "./use-cases/delete-directory/delete-directory.service";
 import { GetDirectoriesDto } from "./use-cases/get-directories/get-directories.dto";
 import { GetDirectoriesService } from "./use-cases/get-directories/get-directories.service";
 
@@ -16,13 +27,16 @@ import { GetDirectoriesService } from "./use-cases/get-directories/get-directori
 export class DirectoriesController {
   private readonly getDirectoriesService: GetDirectoriesService;
   private readonly createDirectoryService: CreateDirectoryService;
+  private readonly deleteDirectoryService: DeleteDirectoryService;
 
   public constructor(
     getDirectoriesService: GetDirectoriesService,
     createDirectoryService: CreateDirectoryService,
+    deleteDirectoryService: DeleteDirectoryService,
   ) {
     this.getDirectoriesService = getDirectoriesService;
     this.createDirectoryService = createDirectoryService;
+    this.deleteDirectoryService = deleteDirectoryService;
   }
 
   @Get()
@@ -39,5 +53,14 @@ export class DirectoriesController {
     @UserDecorator() user: User,
   ) {
     return await this.createDirectoryService.execute(body, user);
+  }
+
+  @Delete(":id")
+  @HttpCode(204)
+  public async deleteDirectory(
+    @Param("id") id: string,
+    @UserDecorator() user: User,
+  ) {
+    return await this.deleteDirectoryService.execute(id, user);
   }
 }
--- backend/src/domain/units/directories/directories.module.ts
+++ backend/src/domain/units/directories/directories.module.ts
@@ -4,10 +4,11 @@ import { Module } from "@nestjs/common";
 import { DirectoriesController } from "./directories.controller";
 import { CreateDirectoryService } from "./use-cases/create-directory/create-directory.service";
 import { GetDirectoriesService } from './use-cases/get-directories/get-directories.service';
+import { DeleteDirectoryService } from './use-cases/delete-directory/delete-directory.service';
 
 @Module({
   controllers: [DirectoriesController],
   imports: [MikroOrmModule.forFeature([Directory])],
-  providers: [CreateDirectoryService, GetDirectoriesService],
+  providers: [CreateDirectoryService, GetDirectoriesService, DeleteDirectoryService],
 })
 export class DirectoriesModule {}
