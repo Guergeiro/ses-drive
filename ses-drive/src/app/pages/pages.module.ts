edaddeb2592da0a8ff5382a570b210812018303e
diogofalken <diogofalken0@gmail.com>
2022-05-28 19:30:38 +0100

    feat: rename directory
    - Rename directory done
    
    - Created dialog to rename directory to be used by files aswell

--- ses-drive/src/app/pages/pages.module.ts
+++ ses-drive/src/app/pages/pages.module.ts
@@ -14,6 +14,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import { AddFolderDialogComponent } from '../components/dialogs/add-folder-dialog/add-folder-dialog.component';
 import { YesOrNoDialogComponent } from '../components/dialogs/yes-or-no-dialog/yes-or-no-dialog.component';
+import { RenameDialogComponent } from '../components/dialogs/rename-dialog/rename-dialog.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,12 @@ import { YesOrNoDialogComponent } from '../components/dialogs/yes-or-no-dialog/y
     PagesComponent,
     AddFolderDialogComponent,
     YesOrNoDialogComponent,
+    RenameDialogComponent,
   ],
-  entryComponents: [AddFolderDialogComponent, YesOrNoDialogComponent],
+  entryComponents: [
+    AddFolderDialogComponent,
+    YesOrNoDialogComponent,
+    RenameDialogComponent,
+  ],
 })
 export class PagesModule {}
